ba40a0c6ea324aa003328bf5b0884d4cffa65ddd
Vojta K <vojta@sherpadevs.com>
2025-07-13 16:44:07 +0200

    Addition of playground repository repository and ackage-lock file, fix of eslist_02

--- playground/eslint.config.mjs
+++ playground/eslint.config.mjs
@@ -2,14 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import stylistic from "@stylistic/eslint-plugin-js";
+import stylistic from "@stylistic/eslint-plugin";
 
 
-export default[
+export default [
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.recommended,
+  pluginReact.configs.flat.recommended,
   stylistic.configs.recommended,
 ];
